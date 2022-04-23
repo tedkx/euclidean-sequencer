5901ebf8283105c2172908354cc919f4648eb41c
Tedk <thodwriskalaitzidis@hotmail.com>
2022-04-23 13:34:26 +0300

    Began drawing circles

--- src/components/PatternDisplay/index.js
+++ src/components/PatternDisplay/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
-const PatternDisplay = ({ patterns }) => {
+const baseRadius = 120;
+const circlePadding = 15;
+const canvasWidth = 360;
+const canvasHeight = 400;
+const canvasCenter = { x: canvasWidth / 2, y: canvasHeight / 2 };
+const inactiveColor = '#383838';
+
+const drawCircle = (ctx, radius, strokeColor) => {
+  ctx.moveTo(canvasCenter.x, canvasCenter.y);
+  ctx.beginPath();
+  ctx.arc(canvasCenter.x, canvasCenter.y, radius, 0, 2 * Math.PI);
+  ctx.strokeStyle = strokeColor;
+  ctx.lineWidth = 1;
+  ctx.stroke();
+};
+
+const PatternDisplay = ({ colorPalette, patterns }) => {
   const canvasRef = React.useRef(null);
 
-  return <canvas ref={canvasRef} />;
+  React.useEffect(() => {
+    if (Array.isArray(patterns)) {
+      const ctx = canvasRef.current.getContext('2d');
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+      for (let i = 0; i < patterns.length; i++) {
+        const { active, steps } = patterns[i];
+        if (Array.isArray(steps)) {
+          drawCircle(
+            ctx,
+            baseRadius - i * circlePadding,
+            active ? colorPalette[i] : inactiveColor
+          );
+        }
+      }
+    }
+  }, [colorPalette, patterns]);
+
+  return <canvas height={canvasHeight} width={canvasWidth} ref={canvasRef} />;
 };
 
 export default PatternDisplay;
--- src/components/Sequencer/View.js
+++ src/components/Sequencer/View.js
@@ -13,14 +13,42 @@ pattern: {
 }
 */
 
-const SequencerView = ({ patterns }) => {
+const colorPalette = [
+  'cyan',
+  'orange',
+  'blue',
+  'red',
+  'green',
+  'purple',
+  'yellow',
+  'pink',
+];
+
+const defaultPatterns = [
+  {
+    active: true,
+    steps: Array.from(Array(16)).map((_, i) => (i % 2 === 0 ? 127 : 0)),
+  },
+  {
+    active: false,
+    steps: [
+      127, 127, 127, 0, 127, 127, 127, 0, 127, 127, 127, 0, 127, 127, 127, 0,
+    ],
+  },
+  {
+    active: true,
+    steps: [127, 0, 0, 0, 127, 0, 0, 0, 127, 0, 0, 0, 127, 0, 0, 0],
+  },
+];
+
+const SequencerView = ({ patterns = defaultPatterns }) => {
   return (
     <Card className="sequencer-container" bordered={false}>
       <div>
         <SequencerSettings />
       </div>
       <div>
-        <PatternDisplay patterns />
+        <PatternDisplay colorPalette={colorPalette} patterns={patterns} />
       </div>
     </Card>
   );
