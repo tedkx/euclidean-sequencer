142d41ebed24448a9a8951468b5ccda50bf404d7
Tedk <thodwriskalaitzidis@hotmail.com>
2022-04-24 22:06:52 +0300

    Added sequencer setting label

--- src/components/SequencerSettings/index.js
+++ src/components/SequencerSettings/index.js
@@ -5,6 +5,10 @@ import SequenceRow from './SequenceRow';
 const SequencerSettings = ({ colorPalette, sequences }) => {
   return (
     <div className="sequencer-settings-container">
+      <div className="settings-labels-container">
+        <div className="setting-label"></div>
+        <div className="setting-label">Steps</div>
+      </div>
       {sequences.map((sequence, idx) => (
         <SequenceRow {...sequence} color={colorPalette[idx]} key={idx} />
       ))}
